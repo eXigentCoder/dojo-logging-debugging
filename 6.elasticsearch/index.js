a6ca7516299553e4ba41d8ebd071b2fc268db96b
Ryan.Kotzen <potz666@gmail.com>
2017-10-11 14:15:07 +0200

    elasticsearch example

--- 6.elasticsearch/index.js
+++ 6.elasticsearch/index.js
@@ -1,20 +1,25 @@
 'use strict';
 const winston = require('winston');
-require('winston-mongodb');
+require('winston-elasticsearch');
+const elasticsearch = require('elasticsearch');
 const levels = require('../shared/levels');
 const colours = require('../shared/colours');
 winston.addColors(colours);
 const logStuff = require('../shared/log-stuff');
 
+const client = new elasticsearch.Client({
+    host: '192.168.99.100:9200'
+});
+
 const logger = new winston.Logger({
     level: 'trace',
     levels: levels,
     transports: [
-        new winston.transports.MongoDB({
-            db:
-                'mongodb://192.168.99.100:27017/my-awesome-app?readPreference=primary',
-            collection: 'my-awesome-logs',
-            level: 'warn'
+        new winston.transports.Elasticsearch({
+            client,
+            indexPrefix: 'awesome-app',
+            mappingTemplate: require('./index-template-mapping.json'),
+            ensureMappingTemplate: true
         })
     ]
 });
