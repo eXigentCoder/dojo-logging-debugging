38bd1ea7198e22e5022131cabcf0c7eec4e8bc74
Ryan.Kotzen <potz666@gmail.com>
2017-10-11 16:41:21 +0200

    Updating template

--- 8.elasticsearch-generate-data/index.js
+++ 8.elasticsearch-generate-data/index.js
@@ -7,10 +7,12 @@ const elasticsearch = require('elasticsearch');
 const levels = require('../shared/levels');
 const Alpine = require('alpine');
 const alpine = new Alpine(Alpine.LOGFORMATS.COMBINED);
-
+const randomGeoHash = require('random-geohash');
+const packageJson = require('../package.json');
 const client = new elasticsearch.Client({
     host: '192.168.99.100:9200'
 });
+const moment = require('moment');
 
 const logger = new winston.Logger({
     level: 'trace',
@@ -19,26 +21,48 @@ const logger = new winston.Logger({
         new winston.transports.Elasticsearch({
             level: 'trace',
             client,
-            indexPrefix: 'awesome-app',
+            indexPrefix: 'app-access-logs',
             mappingTemplate: require('./index-template-mapping.json'),
             ensureMappingTemplate: true,
-            flushInterval: 300
+            flushInterval: 300,
+            transformer
         })
     ]
 });
 
+const totalNumberOfLines = 13509;
 const lineReader = readLine(path.join(__dirname, './access_log'));
 lineReader
-    .on('line', function(line, lineCount, byteCount) {
+    .on('line', function(line, lineCount) {
         const data = alpine.parseLine(line);
-        console.log(data, line);
+        delete data.originalLine;
+        data.geohash = randomGeoHash();
+        data.verb = data.request.split(' ')[0];
+        data.path = data.request.split(' ')[1];
+        data.time = moment(data.time, 'DD/MMM/YYYY:HH:mm:ss Z').toDate();
+        logger.trace(data);
+        if (lineCount === totalNumberOfLines) {
+            setTimeout(function() {
+                // eslint-disable-next-line
+                process.exit(0);
+            }, 1000);
+        }
     })
     .on('error', function(err) {
         throw err;
     });
 
-//
-// setTimeout(function() {
-//     // eslint-disable-next-line
-//     process.exit(0);
-// }, 1000);
+function transformer(logData) {
+    const transformed = {};
+    transformed['@timestamp'] = logData.meta.time;
+    transformed['@version'] = packageJson.version;
+    transformed.severity = logData.level;
+    transformed.ipAddress = logData.meta.remoteHost;
+    transformed.userAgent = logData.meta['RequestHeader User-agent'];
+    transformed.location = logData.meta.geohash;
+    transformed.verb = logData.meta.verb;
+    transformed.path = logData.meta.path;
+    transformed.size = logData.meta.sizeCLF;
+    transformed.status = logData.meta.status;
+    return transformed;
+}
