1ccd2dc2ab16fc914911d9f4c639f929f3bd8c97
Ryan.Kotzen <potz666@gmail.com>
2018-01-18 12:57:30 +0200

    removing colourisation on files, doesn't work.

--- 3.file/index.js
+++ 3.file/index.js
@@ -1,8 +1,6 @@
 'use strict';
 const winston = require('winston');
 const levels = require('../shared/levels');
-const colours = require('../shared/colours');
-winston.addColors(colours);
 const logStuff = require('../shared/log-stuff');
 
 const logger = new winston.Logger({
